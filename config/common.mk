7c52967d687caae095f6c5d0a419c211f8e8dd94
Alberto Ponces <ponces26@gmail.com>
2024-11-01 22:21:50 +0000

    Revert "Revert "common: Switch to AOSPA's face unlock implementation""
    This reverts commit 6171957391abe8712b6d3ade3b96f9b5e4b32964.

--- config/common.mk
+++ config/common.mk
@@ -12,6 +12,16 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
 
+# Face Unlock
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/ponces/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
